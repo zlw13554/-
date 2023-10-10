3364b45992ced2c1be392d52132c5b80092b6375
275258700@qq.com <275258700@qq.com>
2023-10-10 10:25:26 +0800

    提交HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* @program: -
+* @ClassName HelloWorld
+* @description: 
+* @author: zlw
+* @create: 2023-10-10 10:22
+* @Version 1.0
+**/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
